889569ace9c48260344d8141711105b5db0610b3
Minh Duc Nguyen <minh.nguyen@ardc.edu.au>
2020-09-16 14:33:19 +1000

    Added previewing capabilities

--- src/main/java/au/edu/ardc/igsn/igsnportal/controller/ViewController.java
+++ src/main/java/au/edu/ardc/igsn/igsnportal/controller/ViewController.java
@@ -4,7 +4,7 @@ import au.edu.ardc.igsn.igsnportal.model.igsn.Resource;
 import au.edu.ardc.igsn.igsnportal.model.igsn.Resources;
 import au.edu.ardc.igsn.igsnportal.service.IGSNRegistryService;
 import au.edu.ardc.igsn.igsnportal.service.UserService;
-import au.edu.ardc.igsn.igsnportal.util.Helpers;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -33,13 +34,15 @@ public class ViewController {
 
 	/**
 	 * Primary Controller for generating landing page for IGSN
-	 *
-	 * @param request the current {@link HttpServletRequest} to check if the user is logged in
+	 * @param request the current {@link HttpServletRequest} to check if the user is
+	 * logged in
 	 * @param model the {@link Model} for UI rendering
-	 * @param prefix IGSN is in the form of {prefix}/{namespace+value} so this route will capture it
+	 * @param prefix IGSN is in the form of {prefix}/{namespace+value} so this route will
+	 * capture it
 	 * @param namespace the namespace+value field
 	 * @return the thymeleaf template string that will be used
-	 * @throws IOException is thrown when getContentForIdentifierValue failed to generate any plausible result
+	 * @throws IOException is thrown when getContentForIdentifierValue failed to generate
+	 * any plausible result
 	 * @throws ServletException is thrown when failing to check for user logged in status
 	 */
 	@GetMapping("/view/{prefix}/{namespace}")
@@ -53,6 +56,42 @@ public class ViewController {
 
 		// obtain the JSON-LD to embed within the view page
 		String jsonld = service.getContentForIdentifierValue(identifierValue, IGSNRegistryService.ARDCv1JSONLD);
+
+		boolean canEdit = false;
+		if (userService.isLoggedIn(request)) {
+			canEdit = service.canEdit(identifierValue, userService.getPlainAccessToken(request));
+		}
+
+		return renderViewPage(model, identifierValue, xml, jsonld, canEdit);
+	}
+
+	/**
+	 * A method to preview record without having them stored
+	 * @param model the UI {@link Model}
+	 * @param identifierValue request parameter identifierValue for rendering
+	 * @param xml string ardc-igsn-desc-1.0 schema
+	 * @return String of the thymeleaf template
+	 * @throws JsonProcessingException bubbled up exception from
+	 * {{@link #renderViewPage(Model, String, String, String, boolean)}}
+	 */
+	@GetMapping("/preview")
+	public String preview(Model model, @RequestParam String identifierValue, @RequestParam String xml)
+			throws JsonProcessingException {
+		return renderViewPage(model, identifierValue, xml, "", true);
+	}
+
+	/**
+	 * Render the IGSN View Page
+	 * @param model the UI {@link Model}
+	 * @param identifierValue string identifier in the form of {prefix/namespace+value}
+	 * @param xml string ardc-igsn-desc-1.0 schema
+	 * @param jsonld string ardc-json-ld
+	 * @param canEdit boolean value to indicate if the edit button is showing
+	 * @return String of the thymeleaf template
+	 * @throws JsonProcessingException when failing to map xml to {@link Resources}
+	 */
+	private String renderViewPage(Model model, String identifierValue, String xml, String jsonld, boolean canEdit)
+			throws JsonProcessingException {
 		model.addAttribute("jsonld", jsonld);
 
 		logger.debug("Obtained xml from getContentForIdentifierValue");
@@ -70,10 +109,7 @@ public class ViewController {
 		model.addAttribute("identifierValue", identifierValue);
 		model.addAttribute("resource", resources.resource);
 		model.addAttribute("xml", xml);
-		boolean canEdit = false;
-		if (userService.isLoggedIn(request)) {
-			canEdit = service.canEdit(identifierValue, userService.getPlainAccessToken(request));
-		}
+
 		model.addAttribute("igsnURL", "http://igsn.org/" + resources.resource.resourceIdentifier);
 		model.addAttribute("canEdit", canEdit);
 
