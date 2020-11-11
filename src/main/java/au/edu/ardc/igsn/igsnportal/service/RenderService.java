03c02a4268e4ed5f8221bfb0d680343596868608
Liz Woods <lwoods@ANDSs-MacBook-Pro.local>
2020-11-12 09:53:32 +1100

    IGSN-148 added formatting options for YYYY-MM and YYYY date rendering

--- src/main/java/au/edu/ardc/igsn/igsnportal/service/RenderService.java
+++ src/main/java/au/edu/ardc/igsn/igsnportal/service/RenderService.java
@@ -2,22 +2,61 @@ package au.edu.ardc.igsn.igsnportal.service;
 
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 @Service
 public class RenderService {
 
-	public String renderDate(String inputDate) {
-		try {
-			// Parses the date
-			LocalDate dt = LocalDate.parse(inputDate);
-			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM YYYY");
-			return formatter.format(dt);
+	public String renderDate(String inputDate){
+
+		String [] dateParts = inputDate.split("-");
+		Integer formats = dateParts.length;
+		switch (formats) {
+			case 1:
+				//no need to parse we have received either YYYY or unknown string input
+				return inputDate;
+			case 2:
+				try {
+					// Parses the date we have received YYYY-MM
+					YearMonth thisYearMonth = YearMonth.of(Integer.parseInt(dateParts[0]), Integer.parseInt(dateParts[1]));
+					// Create a DateTimeFormatter string
+					DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM yyyy");
+					return thisYearMonth.format(formatter);
+				}
+				catch(Exception ex){
+					return inputDate;
+				}
+			default:
+				try {
+					// Parses the full date
+					LocalDate dt = LocalDate.parse(inputDate);
+					DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM YYYY");
+					return formatter.format(dt);
+				}
+				catch(Exception ex){
+					return inputDate;
+				}
 		}
-		catch (Exception e) {
+	}
+
+	public String renderIdentifier(String identifier) {
+		try{
+			String type = getType(identifier);
+			switch (type){
+				case "doi":
+					return identifier;
+				default:
+					return identifier;
+			}
+		}catch (Exception e) {
 			return null;
 		}
+	}
+
+	public String getType(String identifier){
+		return identifier;
 	}
 
 }
--- src/test/java/au/edu/ardc/igsn/igsnportal/service/RenderServiceTest.java
+++ src/test/java/au/edu/ardc/igsn/igsnportal/service/RenderServiceTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.text.ParseException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -18,10 +20,30 @@ public class RenderServiceTest {
 
 	@Test
 	@DisplayName("When provided with an ISO date string return DD MMM YYYY formatted date string")
-	void testRenderDate() {
+	void testRenderDateYYYYMMDD() throws ParseException {
 
 		String ISODate = "2020-10-28";
 		String expectedFormat = "28 Oct 2020";
+
+		assertThat(renderService.renderDate(ISODate)).isEqualTo(expectedFormat);
+	}
+
+	@Test
+	@DisplayName("When provided with an YYYY-MM string return MMM YYYY formatted date string")
+	void testRenderDateYYYYMM(){
+
+		String ISODate = "2020-10";
+		String expectedFormat = "Oct 2020";
+
+		assertThat(renderService.renderDate(ISODate)).isEqualTo(expectedFormat);
+	}
+
+	@Test
+	@DisplayName("When provided with an YYYY string return YYYY formatted date string")
+	void testRenderDateYYYY() {
+
+		String ISODate = "2020";
+		String expectedFormat = "2020";
 
 		assertThat(renderService.renderDate(ISODate)).isEqualTo(expectedFormat);
 	}
