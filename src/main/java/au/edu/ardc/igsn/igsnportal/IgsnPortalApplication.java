36e9fd0d9ff0ee3f023f182bfb899cb84a71285d
Minh Duc Nguyen <minh.nguyen@ardc.edu.au>
2020-12-08 10:39:17 +1100

    IGSN-293. Added basic 404 and generic error pages

--- src/main/java/au/edu/ardc/igsn/igsnportal/IgsnPortalApplication.java
+++ src/main/java/au/edu/ardc/igsn/igsnportal/IgsnPortalApplication.java
@@ -1,7 +1,9 @@
 package au.edu.ardc.igsn.igsnportal;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
