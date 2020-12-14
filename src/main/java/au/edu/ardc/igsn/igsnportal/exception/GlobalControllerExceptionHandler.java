b9fa73b65d20263ed3b9b0a67e00b3f66310a864
Liz Woods <lwoods@andss-mbp.uds.anu.edu.au>
2020-12-15 10:01:09 +1100

    ensuring the correct editor link in the footer on portal exception response pages

--- src/main/java/au/edu/ardc/igsn/igsnportal/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/au/edu/ardc/igsn/igsnportal/exception/GlobalControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package au.edu.ardc.igsn.igsnportal.exception;
 
+import au.edu.ardc.igsn.igsnportal.config.ApplicationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,12 +11,18 @@ import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
+	final ApplicationProperties applicationProperties;
+
+	public GlobalControllerExceptionHandler(ApplicationProperties applicationProperties) {
+		this.applicationProperties = applicationProperties;
+	}
 
 	@ExceptionHandler({ NotFoundException.class })
 	@ResponseStatus(code = HttpStatus.NOT_FOUND)
 	public String handleNotFound(Exception ex, HttpServletRequest request, Model model) {
 		model.addAttribute("exception", ex);
 		model.addAttribute("url", request.getRequestURL());
+		model.addAttribute("editorLink",applicationProperties.getEditorUrl());
 		return "404";
 	}
 
@@ -24,6 +31,7 @@ public class GlobalControllerExceptionHandler {
 	public String handleUnauthorized(Exception ex, HttpServletRequest request, Model model) {
 		model.addAttribute("exception", ex);
 		model.addAttribute("url", request.getRequestURL());
+		model.addAttribute("editorLink",applicationProperties.getEditorUrl());
 		return "401";
 	}
 
@@ -32,6 +40,7 @@ public class GlobalControllerExceptionHandler {
 	public String handleForbidden(Exception ex, HttpServletRequest request, Model model) {
 		model.addAttribute("exception", ex);
 		model.addAttribute("url", request.getRequestURL());
+		model.addAttribute("editorLink",applicationProperties.getEditorUrl());
 		return "403";
 	}
 
